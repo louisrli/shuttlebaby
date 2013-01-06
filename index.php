80780014b7a1e8b5044a28a1acad9cda73a6ed7c
Louis Li <louis@cato.hcs.harvard.edu>
2013-01-06 18:20:28 -0500

    Final backend completed. No CSS styling.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <?php
-  $stops = unserialize(file_get_contents("http://shuttleboy.cs50.net/api/1.2/stops?output=php"));
-  
+  // See Shuttleboy API for valid stop names
   $raw_src = "Mather House";
   $raw_dest = "Quad";
   
@@ -10,13 +9,16 @@
   
   function get_trips($src_name, $dest_name) {
       $base_api = "http://shuttleboy.cs50.net/api/1.2/trips?output=php";
-      $url_trips = $base_api . "&a=" . $src_name . "&" . "b=" . $dest_name;
+      $url_trips = "{$base_api}&a=" . urlencode($src_name) . "&b=" . urlencode($dest_name);
       return unserialize(file_get_contents($url_trips));
   }
   
   $forward_trips = get_trips($raw_src, $raw_dest);
+  $forward_trips['name'] = "$raw_src to $raw_dest";
+
   $backward_trips = get_trips($raw_dest, $raw_src);
-  
+  $backward_trips['name'] = "$raw_dest to $raw_src";
+
   $all_trips = array($forward_trips, $backward_trips);
   
   // Maximum number of trips to display
@@ -36,14 +38,27 @@
     <div>
       <?php
         foreach ($all_trips as $trip_array) {
-        echo "<table><tr><th>" . title . "</th></tr>";
-        for ($i = 0; i < max(NUM_TRIPS, count($trip_array); i++) {
+    echo "<table><tr><th>" . strtolower($trip_array['name']) . "</th></tr>";
+
+    if (empty($trip_array)) {
+      echo "<tr><td>";
+      echo "No trips found.";
+      echo "</tr></td>";
+    }
+
+    for ($i = 0; $i < min($NUM_TRIPS, count($trip_array)); $i++) {
                 echo "<tr><td>";
-                echo $trip_array[i]["departs"];
+		$next_departure = strtotime($trip_array[$i]["departs"]);
+		echo date("H:i", $next_departure); 
+
+		$min_from_now = round(abs($next_departure - time()) / 60);
+		echo " <span class=\"minortext\">{$min_from_now} min</span>";
+
                 echo "</tr></td>";
-            }
+		
+	}
                  echo "</table>";
-             }
+        }
       ?>
     </div>
   </body>
