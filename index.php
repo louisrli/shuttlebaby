f38f03c510cb3855b79d5fdc00bfd9362f2825a4
Louis Li <louisrli@gmail.com>
2013-01-06 14:53:09 -0800

    Formatted tables correctly.

--- index.php
+++ index.php
@@ -1,61 +1,51 @@
 
 <?php
-$stops = unserialize(file_get_contents("http://shuttleboy.cs50.net/api/1.2/stops?output=php"));
-
-$raw_src = "Mather House";
-$raw_dest = "Quad";
-
-$src = urlencode($raw_src);
-$dest = urlencode($raw_dest);
-
-function get_trips($src_name, $dest_name) {
-  $base_api = "http://shuttleboy.cs50.net/api/1.2/trips?output=php";
-  $url_trips = $base_api . "&a=" . $src_name . "&" . "b=" . $dest_name;
-  return unserialize(file_get_contents($url_trips));
-}
-
-$forward_trips = get_trips($raw_src, $raw_dest);
-$backward_trips = get_trips($raw_dest, $raw_src);
-
-$all_trips = array($forward_trips, $backward_trips);
-
-// Maximum number of trips to display
-$NUM_TRIPS = 3;
+  $stops = unserialize(file_get_contents("http://shuttleboy.cs50.net/api/1.2/stops?output=php"));
+  
+  $raw_src = "Mather House";
+  $raw_dest = "Quad";
+  
+  $src = urlencode($raw_src);
+  $dest = urlencode($raw_dest);
+  
+  function get_trips($src_name, $dest_name) {
+      $base_api = "http://shuttleboy.cs50.net/api/1.2/trips?output=php";
+      $url_trips = $base_api . "&a=" . $src_name . "&" . "b=" . $dest_name;
+      return unserialize(file_get_contents($url_trips));
+  }
+  
+  $forward_trips = get_trips($raw_src, $raw_dest);
+  $backward_trips = get_trips($raw_dest, $raw_src);
+  
+  $all_trips = array($forward_trips, $backward_trips);
+  
+  // Maximum number of trips to display
+  $NUM_TRIPS = 3;
 ?>
 
 <!doctype html>
 <html>
   <head>
-  <title>mather quad</title>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-
+    <title>mather quad</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    
   </head>
   
   <body>
-
-  <div>
-
-  <table>
-     <tr>
-        <th>mather to quad</th>
-        <th>quad to mather</th>
-     </tr>
-
-  <?php
-  foreach ($all_trips as $trip_array) {
-  for ($i = 0; i < max(NUM_TRIPS)
-}
-?>
-     <tr>
-        <td>foobar</td>
-     </tr>
-     <tr>
-        <td>hello</td>
-     </tr>
-  </table>
-
-  <? print_r($trips) ?>
-  </div>
+    
+    <div>
+      <?php
+        foreach ($all_trips as $trip_array) {
+        echo "<table><tr><th>" . title . "</th></tr>";
+        for ($i = 0; i < max(NUM_TRIPS, count($trip_array)) {
+                echo "<tr><td>";
+                echo $trip_array[i]["departs"];
+                echo "</tr></td>";
+            }
+                 echo "</table>";
+             }
+      ?>
+    </div>
   </body>
   
 </html>
